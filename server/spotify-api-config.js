1830a113ec3c85e5cf32461d8a4fe2c14e50894e
Logan Baker <loganbakerdev@gmail.com>
2022-05-03 17:50:02 -0500

    removed secret

--- server/spotify-api-config.js
+++ server/spotify-api-config.js
@@ -2,7 +2,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 
 const spotifyApi = new SpotifyWebApi({
   clientId: 'aff39ac9de5440a4bed8b34bc45ce4f1',
-  clientSecret: '5b28fb3e07744b49af3fb8f8f20e98fe',
+  clientSecret: '',
   redirectUri: 'http://localhost:3000/auth',
 });
 
